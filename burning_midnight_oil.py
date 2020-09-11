2b80d961b6436bebec55b2ccfa28b59329acac1f
Bhavesh <bhavesh189kumar@gmail.com>
2020-09-12 01:19:47 +0530

    Added new problem based on binary search

---
+++ burning_midnight_oil.py
@@ -0,0 +1,24 @@
+#https://codeforces.com/contest/165/problem/B
+def f(v,k):
+    s=0
+    while v!=0:
+        s+=v
+        v//=k
+    return s
+
+def bs(n,k):
+    low=1
+    high=n
+    while low<high:
+        mid=low+high
+        mid//=2
+        ans=f(mid,k)
+        #print(ans,mid,x,low,high)
+        if ans<n:
+            low=mid+1
+        elif ans>=n:
+            high=mid
+    return low
+
+n,k=map(int,input().split())
+print(bs(n,k))
