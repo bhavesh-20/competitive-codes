97259af5a548bfe51ebaa67ff87f240653d33d68
Bhavesh <bhavesh189kumar@gmail.com>
2020-09-14 13:49:47 +0530

    Added new problem maxim_and_discounts

---
+++ maxim_and_discounts.py
@@ -0,0 +1,18 @@
+nq=int(input())
+q=[int(x) for x in input().split()]
+n=int(input())
+a=[int(x) for x in input().split()]
+a.sort(reverse=True)
+m=min(q)
+i=0
+ans=0
+x=0
+while i<n:
+    if x==m:
+        x=0
+        i+=2
+        continue
+    x+=1 
+    ans+=a[i]
+    i+=1
+print(ans)
