01b65b679971d184945aaab3892002487db66ed3
Bhavesh <bhavesh189kumar@gmail.com>
2020-12-23 16:55:52 +0530

    modified python template file

--- template.py
+++ template.py
@@ -25,3 +25,71 @@ stdint =lambda: int(stdin.readline())
 stdpr  =lambda x: stdout.write(str(x))
 
 mod=1000000007
+
+#Complexity: O(max(log2(a), log2(b)))
+def gcd(a,b):
+	while b:
+		a,b=b,a%b
+	return a
+
+#Complexity: O(max(log2(a), log2(b)))
+def lcm(a,b):
+	w=a//gcd(a,b)
+	return w*b
+
+#returns a^b
+#Complexity: O(log2(b))
+def expo(a,b):
+	x,y=1,a
+	while(b>0):
+		if(b&1):
+			x=x*y
+		y=y*y
+		b>>=1
+	return x
+
+#return (a ^ b) mod m
+#Complexity: O(log2(b))
+def power(a,b,m):
+	x,y=1,
+	while(b>0):
+		if(b&1):
+			x=mod(x,y,m)
+		y=mod(y,y,m)
+		b>>=1
+	return x
+
+#returns (a ^ (-1)) mod m
+#works only for m = prime
+#Complexity: O(log2(m))
+def inv(a,m):
+	return power(a,m-2,m)
+
+def mod_inverse(a, n):
+	N = n
+	xx = 0
+	yy = 1
+	y = 0
+	x = 1
+	while(n > 0):
+		q = a // n
+		t = n
+		n = a % n
+		a = t
+		t = xx
+		xx = x - q * xx
+		x = t
+		t = yy
+		yy = y - q * yy
+		y = t
+	x %= N
+	x += N
+	x %= N
+	return x
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
